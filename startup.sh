2cd21149b7945522031e34823671aecb21d59f97
Nicholas Ramsay <nick@yousponsor.me>
2021-06-12 14:54:52 +1000

    added screen install

--- startup.sh
+++ startup.sh
@@ -3,6 +3,10 @@ sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo systemctl mask --now firewalld
 
+# install screen
+sudo dnf install epel-release
+sudo yum install screen
+
 # install nodejs
 sudo yum install nodejs
 
